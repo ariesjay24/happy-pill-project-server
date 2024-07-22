6b6c4afb9a16f2a92cdde91049a56c12405f393f
ariesjay24 <ariescuevas063@gmail.com>
2024-07-22 19:00:55 +0800

    port

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --working-dir=/var/www/html
+echo "Running composer..."
+composer install --optimize-autoloader --no-dev
 
 echo "Caching config..."
 php artisan config:cache
@@ -9,9 +8,15 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Caching views..."
+php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
 
 echo "Running seeders"
 php artisan db:seed --class=AdminSeeder --force
 php artisan db:seed --class=ServiceSeeder --force
+
+echo "Starting Laravel server..."
+php artisan serve --host=0.0.0.0 --port=$PORT
